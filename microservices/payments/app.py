3eb475c7c84d8718ac95743daa7f412c5fe06952
iinesalmeida <60179703+iinesalmeida@users.noreply.github.com>
2025-01-17 21:43:50 +0000

    Update app.py
    Problema da rota resolvido: Foi adicionado Especificação de que o url é /payments

--- microservices/payments/app.py
+++ microservices/payments/app.py
@@ -4,8 +4,9 @@ from controller import payment_controller
 app = Flask(__name__)
 
 # Rota principal
-app.register_blueprint(payment_controller)
-
+#Prof. Inês Almeida - É necessário especificar o prefixo da rota
+#app.register_blueprint(payment_controller)
+app.register_blueprint(payment_controller, url_prefix="/payments")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003)
 
